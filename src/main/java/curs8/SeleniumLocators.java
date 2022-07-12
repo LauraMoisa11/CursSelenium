9baf1e4e60032246a2043ec5e5acf6660adf1aaa
unknown <you@example.com>
2022-07-12 21:33:37 +0300

    Curs9

--- src/main/java/curs8/SeleniumLocators.java
+++ src/main/java/curs8/SeleniumLocators.java
@@ -1,7 +1,13 @@
 package curs8;
 
+
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -16,7 +22,7 @@ public class SeleniumLocators {
 		
 		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 		driver = new ChromeDriver(); 
-		driver.manage().window().maximize();//maximizeaza browserul
+		driver.manage().window().maximize(); //maximizeaza browserul
 		driver.get("https://keybooks.ro/");	
 		
 	}
@@ -28,9 +34,29 @@ public class SeleniumLocators {
 		driver.findElement(By.id("log")).sendKeys("TestUser");
 		driver.findElement(By.id("password")).sendKeys("12345@67890");
 		driver.findElement(By.className("submit_button")).click();
+		Thread.sleep(4000);//opreste executia putin
 		
+		//driver.findElement(ByclassName("user_name)).isDisplayed();
+		//WebEleemnt--> driver.findElement(By.className("user_name"))
+		//isDisplayed()--> metoda selenium care verifica daca un element este prezent sau nu:
+		//intoarce true-->assertTrue()
 		
-		//opreste executia putin
+		WebElement userName = driver.findElement(By.className("user_name"));
+		assertTrue(userName.isDisplayed());
+		
+		//<span class="user_name">Test User</span>
+		
+		String userLogat = userName.getText();
+		System.out.println(userLogat);
+		
+		assertEquals(userName.getText(), "Test User");
+		//assertEquals(userLogat.getText(), "Test User");
+		
+		//assertTrue.driver.findElement(By.className("user_name2")).isDisplayed());
+		assertTrue(driver.findElement(By.className("user_name")).isDisplayed());
+		
+				
+		
 	}
 	
 	@AfterClass
---
+++ src/main/java/curs9/SeleniumLocators.java
@@ -0,0 +1,115 @@
+package curs9;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SeleniumLocators {
+
+WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		
+		System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+		driver = new ChromeDriver(); 
+		driver.manage().window().maximize(); //maximizeaza browserul
+		driver.get("https://keybooks.ro/");	
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+	@Test(priority = 0)
+	public void linkTextLocator() throws InterruptedException {
+		//1 identificam elemente
+		WebElement booksLink = driver.findElement(By.linkText("BOOKS"));
+		//2 actionam asupra elementelor
+		booksLink.click();
+		//3 verificam daca in urma actiunii rezultatul este cel asteptat
+		driver.getCurrentUrl();
+		assertEquals(driver.getCurrentUrl(), "https://keybooks.ro/shop/");
+		
+		Thread.sleep(4000);
+	}
+	@Test(priority = 1)
+	public void partialLinkTextLocator() throws InterruptedException {
+		WebElement cookingBookLink = driver.findElement(By.partialLinkText("Cooking"));
+		cookingBookLink.click();
+		Thread.sleep(4000);
+		WebElement bookTitle = driver.findElement(By.cssSelector("h1[class*='product_title']"));
+		//h1 class="product_title entry-title">Cooking with love</h1>
+		//cssSelector:
+		//h1[class*='product_title']
+		
+		assertTrue(bookTitle.isDisplayed());
+		//una din variante
+		assertEquals(bookTitle.getText(),"Cooking with love");	
+	
+	}
+	@Test(priority = 3)
+	public void classNameLocator() {
+		WebElement price = driver.findElement(By.className("price"));
+		assertTrue(price.isDisplayed());
+		//price.getText()
+
+		String productPrice = price.getText();
+		assertEquals(productPrice, "$20.55");
+	}
+	@Test(priority = 4)
+	public void idLocator() throws InterruptedException {
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("window.scrollBy(0,3000) ");
+		
+		WebElement tabReview = driver.findElement(By.id("tab-title-reviews"));
+		tabReview.click();
+		Thread.sleep(4000);
+		
+		WebElement textAreaComment = driver.findElement(By.id("comment"));
+		assertTrue(textAreaComment.isDisplayed());
+		textAreaComment.sendKeys("Nice book!");
+		
+	}
+	@Test(priority  = 5)
+	public void nameLocator() throws InterruptedException {
+		WebElement nameField = driver.findElement(By.name("author"));
+		nameField.sendKeys("Test");
+		Thread.sleep(4000);
+		nameField.clear();
+		Thread.sleep(4000);
+		nameField.sendKeys("Alt Text");
+		Thread.sleep(4000);
+		
+	}
+	@Test(priority = 6)
+	public void cssLocator() throws InterruptedException {
+		WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+		emailField.sendKeys("test@test.com");
+		Thread.sleep(4000);
+	}
+	@Test(priority = 7)
+	public void xpathLocator() throws InterruptedException {
+		//
+		//cssSelector-->a[class='star-3]
+		//xpath--> //a[@class='star-3]
+		
+		WebElement rating = driver.findElement(By.xpath("//a[@class='star-3']"));
+		rating.click();
+		//<input name="submit" type="submit" id="submit" class="submit" value="Submit">
+		//input[@name=submit']
+		WebElement submitButton = driver.findElement(By.xpath("//input[@name='submit']"));
+		submitButton.click();
+		Thread.sleep(4000);
+		
+		
+	}
+}
